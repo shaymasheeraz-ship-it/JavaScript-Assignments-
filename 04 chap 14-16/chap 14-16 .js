42e940008266692a6e000b955265fae3312e4683
shaymasheeraz-ship-it <shaymasheeraz@gmail.com>
2026-09-11 18:01:39 +0500

    Add chap 14-16

---
+++ 04 chap 14-16/chap 14-16 .js
@@ -0,0 +1,22 @@
+var array = [];
+var myArray = new Array();
+console.log(array, myArray);
+
+
+var array2 = ["Ahmed", "Ali", "Shazia"];
+console.log(array2);
+
+var num = [22, 33, 45];
+console.log(num);
+
+var boolean = [true];
+console.log(boolean);
+
+var mixed = ["Shazia", 10, true, undefined, null];
+console.log(mixed);
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"]
+var naming = [`${numbers[0]}  ${qualifications[0]} <br> ${numbers[1]}  ${qualifications[1]} <br>  ${numbers[2]}  ${qualifications[2]} <br>  ${numbers[3]}  ${qualifications[3]}  <br>  ${numbers[4]}  ${qualifications[4]}  <br>  ${numbers[5]}  ${qualifications[5]}  <br>  ${numbers[6]}  ${qualifications[6]} <br>  ${numbers[7]}  ${qualifications[7]}`]
+document.write(naming);
